b2c10493966f4dad5bab09303de0bb52b4969e10
shaz1718 <shashank.pujari@walchandsangli.ac.in>
2024-07-27 20:32:27 +0530

    Add files via upload

---
+++ bsort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Function to perform bubble sort
+void bubbleSort(vector<int>& arr) {
+    int n = arr.size();
+    bool swapped;
+    for (int i = 0; i < n - 1; ++i) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped)
+            break;
+    }
+}
+
+void printArray(const vector<int>& arr) {
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    vector<int> arr = {64, 25, 12, 22, 11};
+
+    cout << "Original array:\n";
+    printArray(arr);
+
+    bubbleSort(arr);
+
+    cout << "Sorted array:\n";
+    printArray(arr);
+
+    return 0;
+}
